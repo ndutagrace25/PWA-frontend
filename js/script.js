040f64cb8ebcff3369501ac4e592dd2a9b87fb79
Grace Nduta <grace.nduta@mawingunetworks.com>
2020-03-31 18:32:22 +0300

    pagination

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const base_url = "http://localhost:5001/";
+const base_url = "https://destination-backend.mawingunetworks.com/";
 
 // Service worker registration
 if ('serviceWorker' in navigator) {
